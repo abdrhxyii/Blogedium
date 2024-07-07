14a8cbceb345cbfad47c0344f78fbe7a7ae7966c
abdrhxyii <abdrhxyii@git>
2024-07-07 22:29:02 +0530

    build for production updates

--- Blogedium_app/src/app/registration/registration.component.ts
+++ Blogedium_app/src/app/registration/registration.component.ts
@@ -11,8 +11,18 @@ export class RegistrationComponent {
 
   isLogin: boolean = false;
 
-  handleSignup(){
-    console.log("clicked")
+  handleSubmit() {
+    if (this.isLogin) {
+      // Handle sign-in logic
+      console.log('Signing in...');
+    } else {
+      // Handle sign-up logic
+      console.log('Creating account...');
+    }
+  }
+
+  toggleSignupLogin() {
+    this.isLogin = !this.isLogin;
   }
 
 }
