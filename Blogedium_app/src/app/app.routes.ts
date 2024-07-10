9e7b5994b656e7b94e1718f780198abacd3b6ed6
abdrhxyii <abdrhxyii@git>
2024-07-10 16:25:48 +0530

    added blog UI

--- Blogedium_app/src/app/app.routes.ts
+++ Blogedium_app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: "/login", pathMatch: 'full'},
-    {path: "login", component: RegistrationComponent}
+    {path: '', redirectTo: "", pathMatch: 'full'},
+    {path: "login", component: RegistrationComponent},
+    {path: "", component: HomeComponent},
 ];
