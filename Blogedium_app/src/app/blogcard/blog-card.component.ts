a1c278831a2c1cf7f3c05d23e33ec7f4ceb02dae
abdrhxyii <abdrhxyii@git>
2024-07-20 21:58:32 +0530

    added blog card and navbar

---
+++ Blogedium_app/src/app/blogcard/blog-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-card.component.html',
+  styleUrl: './blog-card.component.css'
+})
+export class BlogCardComponent {
+  @Input() imageUrl: string;
+  @Input title: string;
+  @Input description: string;
+  @Input date: string;
+  @Input reads: string;
+  @Input comments: number;
+}
